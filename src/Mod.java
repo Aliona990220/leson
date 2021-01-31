85e8a6cd1282e101989b41eca24ca1babde49e79
Aliona990220 <aliona.havela@gmail.com>
2021-01-31 23:49:33 +0200

    the first program was written with IJ

---
+++ src/Mod.java
@@ -0,0 +1,5 @@
+public class Mod {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/To.java
@@ -0,0 +1,7 @@
+public class To {
+    public static void main(Strings[] args) {
+        for (int i = 1; i < 101; i = i + 1) {
+            System.out.println(i);
+        }
+    }
+}
