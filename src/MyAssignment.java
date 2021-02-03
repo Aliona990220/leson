548c4208163d62b341d39f7240fe4a7853a981c9
Aliona990220 <aliona.havela@gmail.com>
2021-02-03 19:16:48 +0200

    Write through while

--- src/MyAssignment.java
+++
@@ -1,8 +0,0 @@
-public class MyAssignment {
-    public static void main(String[] args) {
-        int i = 3;
-        while (i> 0){
-
-        }
-    }
-}
---
+++ src/WriteThroughWhile.java
@@ -0,0 +1,10 @@
+public class WriteThroughWhile {
+    public static void main(String[] args) {
+        int i = 3;
+        while (i> 0)
+        {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
