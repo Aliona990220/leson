4cb9a5c6e44c327a692d3ac9cf22c90188f5bd07
Aliona990220 <aliona.havela@gmail.com>
2021-02-03 18:53:27 +0200

    Paired and unpaired task number one

---
+++ src/MyAssignment.java
@@ -0,0 +1,5 @@
+public class MyAssignment {
+    public static void main(String[] args) {
+        int i = 3;
+    }
+}
---
+++ src/PairedAndUnpaired.java
@@ -0,0 +1,11 @@
+public class PairedAndUnpaired {
+    public static void main(String[] args) {
+        for(int i = 1;i< 21;i = i+ 1){
+            if (i%2==0){
+                System.out.println("Парне " +i);
+            } else if (i%2!= 0){
+                System.out.println("Непарне " +i);
+            }
+        }
+    }
+}
