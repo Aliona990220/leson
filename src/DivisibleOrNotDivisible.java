7bc6958daaedd3e6a947ca0f1f4b8d85b274d6e4
Aliona990220 <aliona.havela@gmail.com>
2021-02-05 20:37:41 +0200

    Sharing or not sharing done

---
+++ src/DivisibleOrNotDivisible.java
@@ -0,0 +1,12 @@
+public class DivisibleOrNotDivisible {
+    public static void main(String[] args) {
+      for (int i = 20;i< 31;i = i+ 1  ){
+          if (i%2==0){
+              System.out.println(i+ " ділиться");
+          } else {
+              System.out.println(i+ " не ділиться");
+          }
+
+      }
+    }
+}
