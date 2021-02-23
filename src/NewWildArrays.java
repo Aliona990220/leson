3b4489f965e14d7041220e0bbb825f15ad4a5302
Aliona990220 <aliona.havela@gmail.com>
2021-02-23 23:28:24 +0200

    New types of array and old, made of ten pieces

---
+++ src/NewWildArrays.java
@@ -0,0 +1,110 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class NewWildArrays {
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("123", "345", "14", "678");
+        Collections.sort(strings);
+        System.out.println(strings);
+    }
+
+    List<String> arrayList;
+
+    {
+        arrayList = new ArrayList<>();
+        arrayList.add("D");
+        System.out.println();
+
+    }
+    List<String> arrayList2;
+    {
+        arrayList2 = new ArrayList<>();
+        arrayList2.add("I");
+    }
+    List<String> arrayList3;
+    {
+        arrayList3 = new ArrayList<>();
+        arrayList3.add("U");
+    }
+    List<String> arrayList4;
+    {
+        arrayList4 = new ArrayList<>();
+        arrayList4.add("Y");
+    }
+    List<String> arrayList5;
+    {
+        arrayList5 = new ArrayList<>();
+        arrayList5.add("T");
+    }
+    List<String> arrayList6;
+    {
+        arrayList6 = new ArrayList<>();
+        arrayList6.add("R");
+    }
+    List<String> arrayList7;
+    {
+        arrayList7 = new ArrayList<>();
+        arrayList7.add("E");
+    }
+    List<String> arrayList8;
+    {
+        arrayList8 = new ArrayList<>();
+        arrayList8.add("W");
+    }
+    List<String> arrayList9;
+    {
+        arrayList9 = new ArrayList<>();
+        arrayList9.add("Q");
+    }
+    List<String> arrayList10;
+    {
+        arrayList10 = new ArrayList<>();
+        arrayList10.add("Z");
+    }
+    String[] s = {"p", "s", "v"};
+    int[] h = new int[]{7, 8, 4};
+    List<String> strings = Arrays.asList("1", "4");
+    String[] l = {"p", "j", "v"};
+    int[] w = new int[]{7, 0, 4};
+    List<String> stringsp = Arrays.asList("2", "4");
+    String[] pi = {"p", "s", "v"};
+    String[] li = {"p", "j", "p"};
+    int[] e = new int[]{9, 8, 7};
+    List<String> stringso = Arrays.asList("8", "2");
+    String[] oi = {"p", "s", "v"};
+    int[] r = new int[]{7, 8, 3};
+    String[] lp = {"o", "j", "v"};
+    List<String> stringsi = Arrays.asList("8", "3");
+    String[] ii = {"p", "s", "v"};
+    int[] y = new int[]{7, 8, 1};
+    String[] lj = {"p", "j", "x"};
+    List<String> stringsu = Arrays.asList("5", "6");
+    String[] uu = {"p", "s", "v"};
+    int[] u = new int[]{7, 6, 7};
+    String[] lo = {"n", "m", "v"};
+    List<String> stringsk = Arrays.asList("8", "4");
+    String[] kk = {"p", "s", "v"};
+    int[] o = new int[]{9, 8, 6};
+    String[] ld = {"p", "j", "l"};
+    List<String> stringsh = Arrays.asList("8", "9");
+    String[] hh = {"p", "s", "v"};
+    int[] p = new int[]{5, 8, 4};
+    String[] lz = {"p", "u", "z"};
+    List<String> stringsf = Arrays.asList("3", "0");
+    String[] ff = {"p", "s", "v"};
+    int[] i = new int[]{6, 0, 4};
+    String[] ht = {"z", "j", "m"};
+    List<String> stringsl = Arrays.asList("1", "7");
+    String[] ll = {"p", "s", "v"};
+    int[] d = new int[]{7, 8, 4};
+    String[] jh = {"p", "g", "v"};
+    List<String> stringst = Arrays.asList("4", "6");
+    String[] tt = {"p", "s", "v"};
+    int[] j = new int[]{9, 5, 1};
+    String[] kd = {"i", "f", "v"};
+}
+
+
+
