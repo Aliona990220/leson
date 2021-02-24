4997ec8d421614f637e05819c3f55c97952cebf2
Aliona990220 <aliona.havela@gmail.com>
2021-02-24 20:43:06 +0200

    Arrays in four ways subl two, executed

---
+++ src/ArraysFourWayDoubleTwo.java
@@ -0,0 +1,97 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class ArraysFourWayDoubleTwo {
+        public static void main(String[] args) {
+            String[] aa = {"a", "s", "v"};
+            int[] aaa = new int[]{3, 6, 4};
+            List<String> stringsa = Arrays.asList("3");
+            List<String> arrayLista;
+            {
+                arrayLista = new ArrayList<>();
+                arrayLista.add("A");
+            }
+
+            String[] bb = {"p", "j", "v"};
+            int[] bbb = new int[]{7, 0, 4};
+            List<String> stringsb = Arrays.asList("2");
+            List<String> arrayListb;
+            {
+                arrayListb = new ArrayList<>();
+                arrayListb.add("B");
+            }
+
+            String[] cc = {"r", "y", "v"};
+            int[] ccc = new int[]{3, 5, 7};
+            List<String> stringsc = Arrays.asList("5", "2");
+            List<String> arrayListc;
+            {
+                arrayListc = new ArrayList<>();
+                arrayListc.add("C");
+            }
+
+            String[] dd = {"p", "s", "v"};
+            int[] ddd = new int[]{7, 8, 3};
+            List<String> stringsd = Arrays.asList("3", "0");
+            List<String> arrayListd;
+            {
+                arrayListd = new ArrayList<>();
+                arrayListd.add("D");
+            }
+
+            String[] ff = {"i", "j", "z"};
+            List<String> stringsf = Arrays.asList("7", "3");
+            int[] fff = new int[]{7, 8, 3};
+            List<String> arrayListf;
+            {
+                arrayListf = new ArrayList<>();
+                arrayListf.add("T");
+            }
+
+            String[] tt = {"z", "x", "v"};
+            int[] ttt = new int[]{0, 8, 1};
+            List<String> stringst = Arrays.asList("9", "6");
+            List<String> arrayListt;
+            {
+                arrayListt = new ArrayList<>();
+                arrayListt.add("R");
+            }
+
+            String[] ss = {"p", "i", "w"};
+            int[] sss = new int[]{8, 0, 5};
+            List<String> stringss = Arrays.asList("1", "4");
+            List<String> arrayLists;
+            {
+                arrayLists = new ArrayList<>();
+                arrayLists.add("S");
+            }
+
+            String[] gg = {"p", "s", "v"};
+            int[] ggg = new int[]{7, 2, 6};
+            List<String> stringsg = Arrays.asList("1", "9");
+            List<String> arrayListg;
+            {
+                arrayListg = new ArrayList<>();
+                arrayListg.add("G");
+            }
+
+            String[] zz = {"u", "j", "v"};
+            int[] zzz = new int[]{5, 8, 4};
+            List<String> stringsz = Arrays.asList("9", "0");
+            List<String> arrayListz;
+            {
+                arrayListz = new ArrayList<>();
+                arrayListz.add("Z");
+            }
+
+            String[] ww = {"f", "d", "h"};
+            int[] www = new int[]{6, 2, 4};
+            List<String> stringsw = Arrays.asList("3", "7");
+            List<String> arrayListw;
+            {
+                arrayListw = new ArrayList<>();
+                arrayListw.add("W");
+            }
+
+        }
+}
