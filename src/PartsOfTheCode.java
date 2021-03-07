26e870bd494567bef71242b88b64d1cb25267a60
Aliona990220 <aliona.havela@gmail.com>
2021-03-07 21:56:21 +0200

    Other parts of the code, also a two-dimensional array

--- src/PartsOfTheCode.java
+++ src/PartsOfTheCode.java
@@ -1,20 +1,27 @@
 public class PartsOfTheCode {
         public static void main(String[] args) {
-            String birthdays [] = new String[10];
-            birthdays[0] = "Лена Елисеева, 1 марта";
-            birthdays[1] = "Коля Романов, 8 мая";
-            birthdays[2] = "Вика Игнатова, 2 июля";
-            birthdays[3] = "Денис Козлов, 7 сентября";
-            birthdays[4] = "Максим Масленников, 9 ноября";
-            birthdays[5] = "Роман Баранов, 4 августа";
-            birthdays[6] = "Валерия Пяткина, 11 апреля";
-            birthdays[7] = "Олеся Остапенко, 31 января";
-            birthdays[8] = "Костя Гурко, 19 октября";
-            birthdays[9] = "Сережа Наумов, 3 мая";
-            for (int i = 0; i < birthdays.length; i++) {
-                System.out.println(birthdays[i]);
-            }
+//            String birthdays [] = new String[10];
+//            birthdays[0] = "Лена Елисеева, 1 марта";
+//            birthdays[1] = "Коля Романов, 8 мая";
+//            birthdays[2] = "Вика Игнатова, 2 июля";
+//            birthdays[3] = "Денис Козлов, 7 сентября";
+//            birthdays[4] = "Максим Масленников, 9 ноября";
+//            birthdays[5] = "Роман Баранов, 4 августа";
+//            birthdays[6] = "Валерия Пяткина, 11 апреля";
+//            birthdays[7] = "Олеся Остапенко, 31 января";
+//            birthdays[8] = "Костя Гурко, 19 октября";
+//            birthdays[9] = "Сережа Наумов, 3 мая";
+//            for (int i = 0; i < birthdays.length; i++) {
+                int n [] = {23, 6, 8, 79, 34, 90, 1, 15, 9};
+                System.out.println(n.length);
+            int[][] nu = {{4,5,3}, {3,5,6}, {6,9,0}};  // Двомірний масив
+
+            int [][] battle = new int[10][10];   // приклад двомірногомасиву
+            int [][] battle2 = new int[10][10];
+
         }
-}
+        }
+
+
 
 
