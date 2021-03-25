cecac805ad25a4b39c88f4b4ab4e9344af3cf20b
Aliona990220 <aliona.havela@gmail.com>
2021-03-25 21:52:44 +0200

    Still, there was not enough time, not finished yet

--- src/TwentyTry.java
+++ src/TwentyTry.java
@@ -19,7 +19,7 @@ public class TwentyTry {
         ArrayList<String> i2 = new ArrayList<String>();
         i2.add("23");
         i2.add("33");
-        ArrayList<String> p2 = new ArrayList<>();
+        ArrayList<Object> p2 = new ArrayList<Object>();
         p2.add("24");
         p2.add("35");
         ArrayList<Boolean> q2 = new ArrayList<Boolean>();
@@ -27,60 +27,63 @@ public class TwentyTry {
         ArrayList<String> i3 = new ArrayList<String>();
         i3.add("21");
         i3.add("32");
-        ArrayList<String> p3 = new ArrayList<>();
+        ArrayList<Object> p3 = new ArrayList<Object>();
         p3.add("25");
         ArrayList<Boolean> q3 = new ArrayList<Boolean>();
 
         ArrayList<String> i4 = new ArrayList<String>();
         i4.add("27");
         i4.add("37");
-        ArrayList<String> p4 = new ArrayList<>();
+        ArrayList<Object> p4 = new ArrayList<Object>();
         p4.add("28");
         ArrayList<Boolean> q4 = new ArrayList<Boolean>();
 
         ArrayList<String> i5 = new ArrayList<String>();
         i5.add("29");
         i5.add("39");
-        ArrayList<String> p5 = new ArrayList<>();
+        ArrayList<Object> p5 = new ArrayList<Object>();
         p5.add("212");
         ArrayList<Boolean> q5 = new ArrayList<Boolean>();
 
         ArrayList<String> i6 = new ArrayList<String>();
         i6.add("232");
         i6.add("332");
-        ArrayList<String> p6 = new ArrayList<>();
+        ArrayList<Object> p6 = new ArrayList<Object>();
         p6.add("211");
         ArrayList<Boolean> q6 = new ArrayList<Boolean>();
 
         ArrayList<String> i7 = new ArrayList<String>();
         i7.add("255");
         i7.add("344");
-        ArrayList<String> p7 = new ArrayList<>();
+        ArrayList<Object> p7 = new ArrayList<Object>();
         p7.add("266");
         ArrayList<Boolean> q7 = new ArrayList<Boolean>();
 
         ArrayList<String> i8 = new ArrayList<String>();
         i8.add("277");
         i8.add("377");
-        ArrayList<String> p8 = new ArrayList<>();
+        ArrayList<Object> p8 = new ArrayList<Object>();
         p8.add("265");
         ArrayList<Boolean> q8 = new ArrayList<Boolean>();
 
         ArrayList<String> i9 = new ArrayList<String>();
         i9.add("256");
         i9.add("356");
-        ArrayList<String> p9 = new ArrayList<>();
+        ArrayList<Object> p9 = new ArrayList<Object>();
         p9.add("278");
         ArrayList<Boolean> q9 = new ArrayList<Boolean>();
 
         ArrayList<String> i10 = new ArrayList<String>();
         i10.add("287");
         i10.add("387");
-        ArrayList<String> p10 = new ArrayList<>();
+        ArrayList<Object> p10 = new ArrayList<Object>();
         p10.add("290");
         ArrayList<Boolean> q10 = new ArrayList<Boolean>();
 
         String[] a = {"p", "u", "z"};
+        for (int ia = 0; ia < 3; ia++){
+            System.out.println(a[ia]);
+        }
         int[] b = new int[]{0, 8, 3};
         for (int r:b) {
             System.out.println(i);
@@ -88,6 +91,9 @@ public class TwentyTry {
         boolean[] c = new boolean[]{false};
 
         String[] a2 = {"o", "u", "z"};
+        for (int ia2 = 0; ia2 < 3; ia2++){
+            System.out.println(a[ia2]);
+        }
         int[] b1 = new int[]{0, 8, 3};
         for (int r:b1) {
             System.out.println(i);
@@ -95,6 +101,9 @@ public class TwentyTry {
         boolean[] c1 = new boolean[]{true};
 
         String[] a3 = {"h", "u", "z"};
+        for (int ia3 = 0; ia3 < 3; ia3++){
+            System.out.println(a[ia3]);
+        }
         int[] b3 = new int[]{0, 8, 3};
         for (int r:b3) {
             System.out.println(i);
@@ -102,6 +111,9 @@ public class TwentyTry {
         boolean[] c3 = new boolean[]{false};
 
         String[] a4 = {"l", "u", "z"};
+        for (int ia4 = 0; ia4 < 3; ia4++){
+            System.out.println(a[ia4]);
+        }
         int[] b4 = new int[]{0, 8, 3};
         for (int r:b4) {
             System.out.println(i);
@@ -109,6 +121,9 @@ public class TwentyTry {
         boolean[] c4 = new boolean[]{true};
 
         String[] a5 = {"n", "u", "z"};
+        for (int ia5 = 0; ia5 < 3; ia5++){
+            System.out.println(a[ia5]);
+        }
         int[] b5 = new int[]{0, 8, 3};
         for (int r:b5) {
             System.out.println(i);
@@ -116,6 +131,9 @@ public class TwentyTry {
         boolean[] c5 = new boolean[]{false};
 
         String[] a6 = {"m", "u", "z"};
+        for (int ia6 = 0; ia6 < 3; ia6++){
+            System.out.println(a[ia6]);
+        }
         int[] b6 = new int[]{0, 8, 3};
         for (int r:b6) {
             System.out.println(i);
@@ -123,6 +141,9 @@ public class TwentyTry {
         boolean[] c6 = new boolean[]{true};
 
         String[] a7 = {"x", "u", "z"};
+        for (int ia7 = 0; ia7 < 3; ia7++){
+            System.out.println(a[ia7]);
+        }
         int[] b7 = new int[]{0, 8, 3};
         for (int r:b7) {
             System.out.println(i);
@@ -130,13 +151,19 @@ public class TwentyTry {
         boolean[] c7 = new boolean[]{false};
 
         String[] a8 = {"p", "u", "z"};
-        int[] b8 = new int[]{0, 8, 3};
+        for (int ii = 0; ii < 3; ii++){
+            System.out.println(a8[ii]);
+        }
+            int[] b8 = new int[]{0, 8, 3};
         for (int r:b8) {
             System.out.println(i);
         }
         boolean[] c8 = new boolean[]{true};
 
         String[] a9 = {"t", "u", "z"};
+        for (int ia9 = 0; ia9 < 3; ia9++){
+            System.out.println(a[ia9]);
+        }
         int[] b9 = new int[]{0, 8, 3};
         for (int r:b9) {
             System.out.println(i);
@@ -144,6 +171,9 @@ public class TwentyTry {
         boolean[] c9 = new boolean[]{true};
 
         String[] a10 = {"p", "g", "z"};
+        for (int ia0 = 0; ia0 < 3; ia0++){
+            System.out.println(a[ia0]);
+        }
         int[] b10 = new int[]{0, 8, 3};
         for (int r:b10) {
             System.out.println(i);
@@ -151,51 +181,124 @@ public class TwentyTry {
         boolean[] c10 = new boolean[]{true};
 
         List<Integer> e = Arrays.asList(1,2);
-        List<String> s = Arrays.asList();
+        for (Integer j : e) {
+            System.out.println(e);
+        }
+
+        String[] s = { "f", "b"};
+        List<String> w1 = Arrays.asList(s);
+        for (int o = 0; o < 2; o++){
+            System.out.println(s[o]);
+        }
         List<Boolean> x = Arrays.asList(false);
 
         List<Integer> e1 = Arrays.asList(1,3);
-        List<String> s1 = Arrays.asList();
+        for (Integer j : e1) {
+            System.out.println(e1);
+        }
+
+        String[] s1 = { "o", "a"};
+        List<String> w2 = Arrays.asList(s1);
+        for (int o = 0; o < 2; o++){
+            System.out.println(s1[o]);
+        }
         List<Boolean> x1 = Arrays.asList(true);
 
         List<Integer> e2 = Arrays.asList(1,4);
-        List<String> s2 = Arrays.asList();
+        for (Integer j : e2) {
+            System.out.println(e2);
+        }
+
+        String[] s2 = { "f", "z"};
+        List<String> w3 = Arrays.asList(s2);
+        for (int o = 0; o < 2; o++){
+            System.out.println(s2[o]);
+        }
         List<Boolean> x2 = Arrays.asList(false);
 
         List<Integer> e3 = Arrays.asList(5,6);
-        List<String> s3 = Arrays.asList();
+        for (Integer j : e3) {
+            System.out.println(e3);
+        }
+
+        String[] s3 = {"r", "z"};
+        List<String> w4 = Arrays.asList(s3);
+        for (int o = 0; o < 2; o++){
+            System.out.println(s3[o]);
+        }
         List<Boolean> x3 = Arrays.asList(true);
 
         List<Integer> e4 = Arrays.asList(1,8);
-        List<String> s4 = Arrays.asList();
+        for (Integer j : e4) {
+            System.out.println(e4);
+        }
+
+        String[] s4 = {"r", "b"};
+        List<String> w5 = Arrays.asList(s4);
+        for (int o = 0; o < 2; o++){
+            System.out.println(s4[o]);
+        }
         List<Boolean> x4 = Arrays.asList(false);
 
         List<Integer> e5 = Arrays.asList(1,6);
-        List<String> s5 = Arrays.asList();
+        for (Integer j : e5) {
+            System.out.println(e5);
+        }
+
+        String[] s5 = {"r", "z"};
+        List<String> w = Arrays.asList(s5);
+        for (int o = 0; o < 2; o++){
+            System.out.println(s5[o]);
+        }
         List<Boolean> x5 = Arrays.asList(true);
 
         List<Integer> e6 = Arrays.asList(1,9);
-        List<String> s6 = Arrays.asList();
+        for (Integer j : e6) {
+            System.out.println(e6);
+        }
+
+        String[] s6 = {"r", "a"};
+        List<String> w6 = Arrays.asList(s6);
+        for (int o = 0; o < 2; o++){
+            System.out.println(s6[o]);
+        }
         List<Boolean> x6 = Arrays.asList(false);
 
         List<Integer> e7 = Arrays.asList(2,3);
-        List<String> s7 = Arrays.asList();
+        for (Integer j : e7) {
+            System.out.println(e7);
+        }
+
+        String[] s7 = {"b", "a"};
+        List<String> w7 = Arrays.asList(s7);
+        for (int o = 0; o < 2; o++){
+            System.out.println(s7[o]);
+        }
         List<Boolean> x7 = Arrays.asList(true);
 
         List<Integer> e8 = Arrays.asList(2,4);
-        List<String> s8 = Arrays.asList();
+        for (Integer j : e8) {
+            System.out.println(e8);
+        }
+
+        String[] s8 = {"o", "z"};
+        List<String> w8 = Arrays.asList(s8);
+        for (int o = 0; o < 2; o++){
+            System.out.println(s8[o]);
+        }
         List<Boolean> x8 = Arrays.asList(false);
 
         List<Integer> e9 = Arrays.asList(2,5);
-        List<String> s9 = Arrays.asList();
+        for (Integer j : e9) {
+            System.out.println(e9);
+        }
+
+        String[] s9 = {"f", "a"};
+        List<String> w9 = Arrays.asList(s8);
+        for (int o = 0; o < 2; o++){
+            System.out.println(s9[o]);
+        }
         List<Boolean> x9 = Arrays.asList(true);
-
-
-
-
-
-
-
 
     }
 }
