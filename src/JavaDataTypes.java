d0770e3e4fd1be29b3844d9619397593783d0841
Aliona990220 <aliona.havela@gmail.com>
2021-03-28 21:37:42 +0200

    All that I could, forgive so little

--- src/JavaDataTypes.java
+++ src/JavaDataTypes.java
@@ -1,41 +1,115 @@
+import java.util.ArrayList;
+
 public class JavaDataTypes {
     public static void main(String[] args) {
-        byte[] v = new byte[]{1,2,3,4,5,6,7};
-        for (byte h : v){
+        byte[] v = new byte[]{1, 2, 3, 4, 5, 6, 7};
+        for (byte h : v) {
             System.out.println(h);
         }
-        short[] w = new short[]{7,8,9,10,11,2};
-        for (int i = 0; i < 5; i++){
+        short[] w = new short[]{7, 8, 9, 10, 11, 2};
+        for (int i = 0; i < 5; i++) {
             System.out.println(w[i]);
         }
-        int[] t = new int[]{23,24,25,27,34};
-        for (int o = 0; o < t.length; o++){
+        int[] t = new int[]{23, 24, 25, 27, 34};
+        for (int o = 0; o < t.length; o++) {
             System.out.println(t[o]);
         }
-        long[] l = new long[]{23454,345665,5484,59859853};
-        for (long q : l){
+        long[] l = new long[]{23454, 345665, 5484, 59859853};
+        for (long q : l) {
             System.out.println(q);
         }
-        char[] c = new char[]{'Y','X','Z'};
+        char[] c = new char[]{'Y', 'X', 'Z'};
         for (int m = 0; m < c.length; m++) {
             System.out.println(c[m]);
         }
-        float[] f = new float[]{2989.48f,6045.58f};
-        for (int i = 0; i < 2; i++){
+        float[] f = new float[]{2989.48f, 6045.58f};
+        for (int i = 0; i < 2; i++) {
             System.out.println(f[i]);
         }
-        double[] d = new double[]{850840.648,950594.9049};
-        for (double k : d){
+        double[] d = new double[]{850840.648, 950594.9049};
+        for (double k : d) {
             System.out.println(k);
         }
-        boolean[] b = new boolean[]{true,false,true};
-        for (int u = 0; u < 2; u++){
+        boolean[] b = new boolean[]{true, false, true};
+        for (int u = 0; u < 2; u++) {
             System.out.println(b[u]);
         }
-        String[] j = new String[]{"ЗИМА","ОСІНЬ","ВЕСНА","ЛІТО"};
+        String[] j = new String[]{"ЗИМА", "ОСІНЬ", "ВЕСНА", "ЛІТО"};
         for (int s = 0; s < j.length; s++) {
             System.out.println(j[s]);
         }
-    }
+        ArrayList<Boolean> l1 = new ArrayList<Boolean>();
+        l1.add(false);
+        l1.add(true);
+        l1.add(false);
+        for (boolean o : l1) {
+            System.out.println(o);
+        }
+//        ArrayList l2 = new ArrayList<Short>();
+//        l2.add(14);
+//        l2.add(2);
+//        l2.add(43);
+//        for (short m : l2) {
+//            System.out.println(m);
+//        }
+        ArrayList<Integer> l3 = new ArrayList<Integer>();
+        l3.add(11);
+        l3.add(12);
+        l3.add(45);
+        l3.add(446);
+        for (Integer h : l3) {
+            System.out.println(h);
+        }
+        ArrayList<Integer> l4 = new ArrayList<Integer>();
+        l4.add(111);
+        l4.add(123);
+        l4.add(409);
+        l4.add(446);
+        l4.add(44454);
+        l4.add(469595);
+        l4.add(4050234);
+        l4.add(445435390);
+        for (int p = 0; p < l4.size(); p++) {
+            System.out.println(l4.get(p));
+        }
+        ArrayList<String> l5 = new ArrayList<String>();
+        l5.add("Колесо");
+        l5.add("Сокира");
+        l5.add("Пила");
+        l5.add("Мотузка");
+        l5.add("Дріт");
+        l5.add("Мило");
+        l5.add("Арматура");
+        l5.add("Кий");
+        l5.add("Культяпка");
+        for (String p : l5) {
+            System.out.println(p);
+        }
+        ArrayList<Float> l6 = new ArrayList<Float>();
+        l6.add(1.45f);
+        l6.add(123.4f);
+        l6.add(40.67f);
+        l6.add(4.6f);
+        l6.add(4.54f);
+        l6.add(47.95f);
+        l6.add(40.34f);
+        l6.add(4.443f);
+        for (float p : l6) {
+            System.out.println(p);
+        }
+        ArrayList<Double> l7 = new ArrayList<Double>();
+        l7.add(1.4);
+        l7.add(24324.5);
+        l7.add(6959.6);
+        l7.add(654.676);
+        l7.add(7887.87);
+        l7.add(1.34);
+        l7.add(123.3);
+        for (double k : l7) {
+            System.out.println(k);
+        }
 
+    }
 }
+
+
