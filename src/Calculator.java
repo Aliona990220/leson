9d8357749f69c64840d47281d8539d45395ed78b
Aliona990220 <aliona.havela@gmail.com>
2021-04-12 20:36:24 +0200

    Calculator part too

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,18 +1,26 @@
 import java.util.Scanner;
-
 public class Calculator {
     public static void main(String[] args) {
         Scanner v = new Scanner(System.in);
-        int a = 5;
-        int b = 10;
-        String z = "+";
-        System.out.print("Введіть число a : ");
-        String m = v.nextLine();
-        System.out.println("Введіть дію z : ");
-        String n = v.nextLine();
-        System.out.println("Введіть число b : ");
-        String k = v.nextLine();
-        System.out.println("результат :");
-            System.out.println("15");
-        }
+        System.out.println("Введіть число");
+        int a = v.nextInt();
+        int b = v.nextInt();
+        int c = a + b;
+        System.out.println("результат" + c);
+        System.out.println("Введіть число");
+        int r = v.nextInt();
+        int t = v.nextInt();
+        int d = r - t;
+        System.out.println("результат" + d);
+        System.out.println("Введіть число");
+        int e = v.nextInt();
+        int q = v.nextInt();
+        int w = e * q;
+        System.out.println("результат" + w);
+        System.out.println("Введіть число");
+        int i = v.nextInt();
+        int o = v.nextInt();
+        int p = i / o;
+        System.out.println("результат" + p);
     }
+}
