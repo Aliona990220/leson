38bcc6538692ddd8d8b2673e8a3dfe873b6fe036
Aliona990220 <aliona.havela@gmail.com>
2021-06-18 18:59:33 +0200

    Third changes in the assignment, three hours lesson.

--- .idea/src/main/java/Main.java
+++ .idea/src/main/java/Main.java
@@ -1,7 +1,9 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -10,25 +12,32 @@ import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         System.setProperty("webdriver.chrome.driver", "C:\\WebDrayver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-//        WebElement element = (new WebDriverWait(driver, Duration.ofSeconds(10))
-//                .until(ExpectedConditions.presenceOfElementLocated(By.id("123"))));
-
-        driver.get("https://www.udemy.com/");
-        WebElement element = driver.findElement(By.xpath("//input[@name='q']"));
-
-        String par = element.getCssValue("transition");
-        System.out.println(par);
-
-        element.click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 
 
-//        WebElement input = driver.findElement(By.xpath("//input[@aria-label='Найти']"));
+
+        try {
+            driver.get("https://crossbrowsertesting.github.io/drag-and-drop");
+            Thread.sleep(2000);
+            WebElement element = driver.findElement(By.id("draggable"));
+            WebElement element2 = driver.findElement(By.id("droppable"));
+
+            Actions actions = new Actions(driver);
+
+            actions.moveToElement(element).clickAndHold().moveToElement(element2).release().build().perform();
+
+
+        } catch (InterruptedException e) {
+
+            e.printStackTrace();
+        }
+
+
+
 
 
 
