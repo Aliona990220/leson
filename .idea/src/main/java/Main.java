465a58287ba0d6384626080df5f7a648e9a1488a
Aliona990220 <aliona.havela@gmail.com>
2021-06-18 20:09:05 +0200

    Fourth changes in the assignment, three hours lesson.

--- .idea/src/main/java/Main.java
+++ .idea/src/main/java/Main.java
@@ -9,35 +9,39 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\WebDrayver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-
-
-
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
         try {
-            driver.get("https://crossbrowsertesting.github.io/drag-and-drop");
+            driver.get("https://pagination.js.org/");
             Thread.sleep(2000);
-            WebElement element = driver.findElement(By.id("draggable"));
-            WebElement element2 = driver.findElement(By.id("droppable"));
 
-            Actions actions = new Actions(driver);
+            List<WebElement> elements = driver.findElements(By.xpath("//div[@class='data-container']/ul/li"));
+            List<WebElement> pages = driver.findElements(By.xpath("//div[@class='paginationjs-pages']/ul/li"));
+            String text = elements.get(5).getText();
+            System.out.println(text);
 
-            actions.moveToElement(element).clickAndHold().moveToElement(element2).release().build().perform();
+            pages.get(2).click();
+            wait.until(ExpectedConditions.stalenessOf(elements.get(5)));
 
+            elements = driver.findElements(By.xpath("//div[@class='data-container']/ul/li"));
+
+            text = elements.get(5).getText();
+            System.out.println(text);
 
         } catch (InterruptedException e) {
-
             e.printStackTrace();
-        }finally {
-            Thread.sleep(20000);
+        } finally {
             driver.quit();
         }
+
+
 
 
 
